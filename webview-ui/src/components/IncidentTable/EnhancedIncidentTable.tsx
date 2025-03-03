6fba2669a97a6d44abe37776eebc7b9b5448606b
David Zager <dzager@redhat.com>
2025-03-03 11:46:32 -0500

    foobar
    Signed-off-by: David Zager <dzager@redhat.com>

--- webview-ui/src/components/IncidentTable/EnhancedIncidentTable.tsx
+++ webview-ui/src/components/IncidentTable/EnhancedIncidentTable.tsx
@@ -1,4 +1,5 @@
 import "./incidentTable.css";
+import { basename } from "node:path"
 import React, { FC, useState } from "react";
 import {
   Badge,
@@ -8,10 +9,6 @@ import {
   CardTitle,
   LabelGroup,
   Label,
-  // Toolbar,
-  // ToolbarContent,
-  // ToolbarGroup,
-  // ToolbarItem,
   Dropdown,
   MenuToggleElement,
   MenuToggle,
@@ -22,12 +19,15 @@ import {
   ToolbarContent,
   ToolbarGroup,
   ToolbarItem,
+  SearchInput,
+  Select,
+  SelectList,
+  SelectOption,
 } from "@patternfly/react-core";
 import { EnhancedIncident, Incident } from "@editor-extensions/shared";
 import { Table, Tr, Tbody, Td } from "@patternfly/react-table";
 import { useExtensionStateContext } from "../../context/ExtensionStateContext";
 import { openFile } from "../../hooks/actions";
-// import GetSolutionDropdown from "../GetSolutionDropdown";
 import { getIncidentRelativePath } from "../../utils/incident";
 import GetSolutionDropdown from "../GetSolutionDropdown";
 
@@ -45,6 +45,73 @@ export const EnhancedIncidentTable: FC<EnhancedIncidentTableProps> = ({
   const [selectedIncidents, setSelectedIncidents] = useState<EnhancedIncident[]>([]);
   const [bulkSelection, setBulkSelection] = React.useState("");
   const [isBulkSelectDropdownOpen, setIsBulkSelectDropdownOpen] = useState<boolean>(false);
+  const { workspaceRoot } = state;
+
+  // Ruleset handling
+  const [rulesetSelected, setRulesetSelected] = useState("");
+  const [rulesetExpanded, setRulesetExpanded] = useState(false);
+  const onRulesetToggle = () => {
+    setRulesetExpanded((prevState) => !prevState);
+  };
+  const onRulesetSelect = (
+    _event: React.MouseEvent | undefined,
+    selection: string | number | undefined,
+  ) => {
+    setRulesetSelected(selection as string);
+    setRulesetExpanded(false);
+  };
+  const rulesetOptions = [...new Set(incidents.map((i) => i.ruleset_name))];
+  console.log(rulesetOptions);
+  // end ruleset handling
+
+  // category handling
+  const [categorySelected, setCategorySelected] = useState("");
+  const [categoryExpanded, setCategoryExpanded] = useState(false);
+  const onCategoryToggle = () => {
+    setCategoryExpanded((prevState) => !prevState);
+  };
+  const onCategorySelect = (
+    _event: React.MouseEvent | undefined,
+    selection: string | number | undefined,
+  ) => {
+    setCategorySelected(selection as string);
+    setCategoryExpanded(false);
+  };
+  const categoryOptions = [...new Set(incidents.map((i) => i.violation_category))];
+  console.log(categoryOptions);
+  // end category handling
+
+  // labels handling
+  const [labelSelected, setLabelSelected] = useState("");
+  const [labelExpanded, setLabelExpanded] = useState(false);
+  const onLabelToggle = () => {
+    setLabelExpanded((prevState) => !prevState);
+  };
+  const onLabelSelect = (
+    _event: React.MouseEvent | undefined,
+    selection: string | number | undefined,
+  ) => {
+    setLabelSelected(selection as string);
+    setLabelExpanded(false);
+  };
+  const labelOptions = [...new Set(incidents.flatMap((i) => i.violation_labels))];
+
+  // incident uri handling
+  const [fileSelected, setFileSelected] = useState("");
+  const [fileExpanded, setFileExpanded] = useState(false);
+  const onFileToggle = () => {
+    setFileExpanded((prevState) => !prevState);
+  };
+  const onFileSelect = (
+    _event: React.MouseEvent | undefined,
+    selection: string | number | undefined,
+  ) => {
+    setFileSelected(selection as string);
+    setFileExpanded(false);
+  };
+
+  const fileOptions = [...new Set(incidents.map((i) => getIncidentRelativePath(i, workspaceRoot)))];
+
   const setIncidentSelected = (row: EnhancedIncident, isSelecting: boolean) =>
     setSelectedIncidents((prevSelected) => {
       const otherSelectedIncidents = prevSelected.filter((r) => {
@@ -52,13 +119,18 @@ export const EnhancedIncidentTable: FC<EnhancedIncidentTableProps> = ({
       });
       return isSelecting ? [...otherSelectedIncidents, row] : otherSelectedIncidents;
     });
+  const [filters, setFilters] = useState({
+    uri: "",
+    ruleset_name: "",
+    category: "",
+    label: "",
+  });
 
   const selectAllIncidents = (isSelecting: boolean) =>
     setSelectedIncidents(isSelecting ? incidents.map((r) => r) : []);
 
   const isIncidentSelected = (row: EnhancedIncident) => selectedIncidents.includes(row);
 
-  const { workspaceRoot } = state;
   const handleIncidentSelect = (incident: Incident) => {
     setFocusedIncident(incident);
     dispatch(openFile(incident.uri, incident.lineNumber ?? 0));
@@ -93,15 +165,6 @@ export const EnhancedIncidentTable: FC<EnhancedIncidentTableProps> = ({
     return (
       <Dropdown
         role="menu"
-        // onSelect={(_event: React.MouseEvent<Element, MouseEvent>, value: string) => {
-        //   if (value === 'all') {
-        //     selectAllIncidents(bulkSelection !== 'all');
-        //   }
-        //   } else {
-        //     setSelectedIncidents([]);
-        //   }
-        //   setBulkSelection(value as string);
-        // }}
         isOpen={isBulkSelectDropdownOpen}
         onOpenChange={(isOpen: boolean) => setIsBulkSelectDropdownOpen(isOpen)}
         toggle={(toggleRef: React.Ref<MenuToggleElement>) => (
@@ -135,12 +198,126 @@ export const EnhancedIncidentTable: FC<EnhancedIncidentTableProps> = ({
     );
   };
 
+  // Handle filter changes
+  const handleFilterChange = (key, value) => {
+    setFilters((prev) => ({ ...prev, [key]: value }));
+  };
+
+  // Filter incidents based on the filters
+  const filteredIncidents = incidents.filter((incident) => {
+    return (
+      (!filters.uri || incident.uri.includes(filters.uri)) &&
+      (!filters.ruleset_name || incident.ruleset_name?.includes(filters.ruleset_name)) &&
+      (!filters.category || incident.violation_category?.includes(filters.category)) &&
+      (!filters.label || incident.violation_labels?.some((label) => label.includes(filters.label)))
+    );
+  });
+
   return (
     <>
       <Toolbar>
         <ToolbarContent>
           <ToolbarGroup>
             <ToolbarItem>{buildBulkSelectDropdown()}</ToolbarItem>
+          </ToolbarGroup>
+          <ToolbarItem>
+            <SearchInput placeholder="Search incidents" />
+          </ToolbarItem>
+          <ToolbarGroup variant="filter-group">
+            <ToolbarItem>
+              <Select
+                role="menu"
+                toggle={(toggleRef: React.Ref<MenuToggleElement>) => (
+                  <MenuToggle
+                    ref={toggleRef}
+                    onClick={onRulesetToggle}
+                    isExpanded={rulesetExpanded}
+                  >
+                    Ruleset
+                  </MenuToggle>
+                )}
+                isOpen={rulesetExpanded}
+                onSelect={onRulesetSelect}
+                onOpenChange={(isOpen) => setRulesetExpanded(isOpen)}
+                selected={rulesetExpanded}
+              >
+                <SelectList>
+                  {rulesetOptions.map((name) => (
+                    <SelectOption key={name}>{name}</SelectOption>
+                  ))}
+                </SelectList>
+              </Select>
+            </ToolbarItem>
+            <ToolbarItem>
+              <Select
+                role="menu"
+                toggle={(toggleRef: React.Ref<MenuToggleElement>) => (
+                  <MenuToggle
+                    ref={toggleRef}
+                    onClick={onCategoryToggle}
+                    isExpanded={categoryExpanded}
+                  >
+                    Category
+                  </MenuToggle>
+                )}
+                isOpen={categoryExpanded}
+                onSelect={onCategorySelect}
+                onOpenChange={(isOpen) => setCategoryExpanded(isOpen)}
+                selected={categoryExpanded}
+              >
+                <SelectList>
+                  {categoryOptions.map((name) => (
+                    <SelectOption key={name}>{name}</SelectOption>
+                  ))}
+                </SelectList>
+              </Select>
+            </ToolbarItem>
+            <ToolbarItem>
+              <Select
+                role="menu"
+                toggle={(toggleRef: React.Ref<MenuToggleElement>) => (
+                  <MenuToggle ref={toggleRef} onClick={onLabelToggle} isExpanded={labelExpanded}>
+                    Labels
+                  </MenuToggle>
+                )}
+                isOpen={labelExpanded}
+                onSelect={onLabelSelect}
+                onOpenChange={(isOpen) => setLabelExpanded(isOpen)}
+                selected={labelExpanded}
+              >
+                <SelectList>
+                  {labelOptions.map((name) => (
+                    <SelectOption key={name}>{name}</SelectOption>
+                  ))}
+                </SelectList>
+              </Select>
+            </ToolbarItem>
+            <ToolbarItem>
+              <Select
+                role="menu"
+                toggle={(toggleRef: React.Ref<MenuToggleElement>) => (
+                  <MenuToggle ref={toggleRef} onClick={onFileToggle} isExpanded={fileExpanded}>
+                    Files
+                  </MenuToggle>
+                )}
+                popperProps={{
+                  appendTo: document.body,
+                  position: "right",
+                  enableFlip: true,
+                  preventOverflow: true,
+                }}
+                isOpen={fileExpanded}
+                onSelect={onFileSelect}
+                onOpenChange={(isOpen) => setFileExpanded(isOpen)}
+                selected={setFileSelected}
+              >
+                <SelectList>
+                  {fileOptions.map((name) => (
+                    <SelectOption key={name}>{name}</SelectOption>
+                  ))}
+                </SelectList>
+              </Select>
+            </ToolbarItem>
           </ToolbarGroup>
           <ToolbarItem align={{ default: "alignEnd" }}>
             <GetSolutionDropdown incidents={selectedIncidents} scope="in-between" />
